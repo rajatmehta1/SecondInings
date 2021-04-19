cde33f9c059e9804fbab14374901958c8896b882
tekshila <67528077+tekshila@users.noreply.github.com>
2021-04-19 16:22:07 -0400

    updated.

---
+++ si-ui/src/main/java/com/si/SiUiApplication.java
@@ -0,0 +1,13 @@
+package com.si;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiUiApplication.class, args);
+    }
+
+}
